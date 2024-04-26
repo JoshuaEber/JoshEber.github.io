2e562e97a4d8bd01ca241e5dd97d2c201fe4092c
JoshuaEber <144479301+JoshuaEber@users.noreply.github.com>
2024-04-26 11:59:05 -0400

    Add files via upload

---
+++ Chapter02/js02/project01/project02-01_txt.js
@@ -0,0 +1,11 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-01
+
+      Celsius <-> Farenheit Coverter
+      Author: Josh Eber
+      Date:   4/11/2024
+
+      Filename: project02-01.js
+ */
+
