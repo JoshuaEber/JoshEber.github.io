9eddce50c472f1b99dca7674ae52196fa2b5092f
JoshuaEber <144479301+JoshuaEber@users.noreply.github.com>
2024-05-03 11:42:38 -0400

    Add files via upload

---
+++ Chapter03/js03/chapter/js03_txt.js
@@ -0,0 +1,12 @@
+/*    JavaScript 7th Edition
+     Chapter 3
+     Chapter case
+
+     Tipton Turbines
+     Program to display games results in a web table
+     Author: Josh Eber
+     Date:   4/17/2024
+
+     Filename: js03.js
+ */
+
